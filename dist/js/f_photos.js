9581b16c5b537a1d9fde0cb891f345266a3fbef4
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-04-02 12:58:08 +0800

    Add jQuery UI link for slider 1.append jQuery UI for toggle function and parameter ({direction:'right'}) 2.js adjust event on button. After the current picture hide then trigger the next picture show.

--- dist/js/f_photos.js
+++ dist/js/f_photos.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-    var Acctoken = 'EAACEdEose0cBALwNYgM4bluJIaDOjecNVu0agj2lWQOcf7CR8CwfKq1Pf1Cbfk8FB5mgmCMFEjkJbanShGD9okTAvIUeEVKXpdVSJE7zkDKfN06aqZB0b7Pm8IXH4m1RiRcQ8MvZAuCwFK4C7HFudUf285dXPawwCw2SKErZAQ7ZCzeCLZBny72DZCx1rPPogZD';
+    var Acctoken = 'EAACEdEose0cBABVPM6qD3uQ8KbnEewIf9LS4APzQRxtoXDUZB6vOTzWYo4cwr7YBPIapMLyFiUfT3csUrv8xWUs7wEJqczQSnM3MdVwKJFuujmedebKyQvdnust9QZCnKmxUqMwx3wcx06MCtWXHibbAiPuXN5htQk5OxAhRD1G3kdIKzdAcZC7PmIDsuAZD';
     var fb_photos_url = 'https://graph.facebook.com/v2.1/me?';
     var fields = 'albums{photos{images,name,likes}}';
+    var galleryIDs = ['#img'];
+    // galleryIDs.push('#img2');
+    var curID = 0;
     $.ajax({
             url: fb_photos_url,
             type: 'GET',
             dataType: 'json',
             data: { access_token: Acctoken, fields, fields },
         })
+        // .beforeSend(function(){
+        // 	$('section').html(>'')
+        // }),
         .done(function(response) {
             console.log("success");
             //find 'Life is a story' album
@@ -19,24 +25,38 @@ $(document).ready(function() {
                 }
             }
         })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
+        .fail(function(msg) {
+            console.log("error" + msg);
         });
+    // .always(function() {
+    //     console.log("complete");
+    // });
 
     var gallery = function(photos) {
-        for (index in photos) {
-            var id = photos[index].id;
-            var src = photos[index].images[0].source;
-            var name = photos[index].name;
-            var tag_img = '<img src="' + src + '" alt="' + src + '" />';
-            var tag_article = '<article class="content">' + name.replace(/\n/g, "<br />") + '</article>';
-            $('section').append('<div class="content" ' + 'id="' + id + '">' + tag_img  + tag_article+ '</div>');
+            for (index in photos) {
+                var id = photos[index].id;
+                galleryIDs.push('#' + id);
+                var src = photos[index].images[0].source;
+                var name = photos[index].name;
+                var tag_img = '<img src="' + src + '" alt="' + src + '" />';
+                var tag_article = '<article class="content">' + name.replace(/\n/g, "<br />") + '</article>';
+                $('section').append($('<div class="content" ' + 'id="' + id + '">' + tag_img + tag_article + '</div>').hide());
+            }
         }
-    }
+        // $('#img2').hide();
+    $('button.next').on('click', function() {
+        $(galleryIDs[curID++]).toggle('drop', { direction: 'left' }, 500, function() {
+            $(galleryIDs[curID]).toggle('drop', { direction: 'right' }, 500);
+        });
+        curID %= galleryIDs.length;
+    });
+    $('button.prev').on('click', function(callback) {
+        $("#img").toggle('drop', { direction: 'left' }, 500, function() {
+            $("#img2").toggle('drop', { direction: 'right' }, 500, function() {
+                console.log("IM2");
+
+            });
+        });
+    });
 
 });
-
-
