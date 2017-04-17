9bb7ba78c2782aae99acedc6aaa8b889c601dc1e
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-04-17 16:14:59 +0800

    Remove click on nav event 1.move the event to hover in css

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,10 +1,3 @@
 $(document).ready(function() {
-    $("#design ul").hide();
-    $("#design").on('click', function(event) {
-        event.preventDefault();
-        /* Act on the event */
-        $(this).children('ul').slideToggle('slow', function() {
 
-        });
-    });
 });
