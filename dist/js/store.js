fd31de26485ea480d8c1af299938517858e52feb
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-04-29 18:00:39 +0800

    add live form

--- dist/js/store.js
+++ dist/js/store.js
@@ -3,7 +3,17 @@
         'name': 'Yellow Cat',
         'price': 200,
         'description': 'the truly gold.',
-        'review': 'so cute',
+        reviews: [{
+            stars: 5,
+            body: "I love this gem!",
+            author: "joe@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 1,
+            body: "This gem sucks.",
+            author: "tim@example.org",
+            createdOn: 1397490980837
+        }],
         'specification': 'meou~',
         'canPurchase': true,
         'soldOut': false,
@@ -11,9 +21,30 @@
             '../img/cat1.jpg', '../img/cat2.jpg'
         ],
     }, {
-        'name': 'medal',
+        'name': 'Fish ',
+        'price': 50,
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        reviews: [{
+            stars: 5,
+            body: "Praesent cursus elit in turpis iacu",
+            author: "joe@example.org",
+            createdOn: 1397490980837
+        }, {
+            stars: 1,
+            body: "This gem sucks.",
+            author: "tim@example.org",
+            createdOn: 1397490980837
+        }],
+        'specification': 'nisi 40*rr',
+        'canPurchase': true,
+        'soldOut': false,
+        'images': [
+            '../img/fish1.jpg', '../img/fish2.jpg'
+        ],
+    },{
+        'name': 'dog',
         'price': 700,
-        'description': 'the truly medal.',
+        'description': 'Donec sodales lorem vel mauris consectetur, at vestibulum neque faucibus.',
         'canPurchase': false,
         'soldOut': true,
         'images': [
@@ -29,9 +60,17 @@
         this.setTab = function(NewTab) {
             this.tab = NewTab;
         };
-        this.selectTab = function(checkTab){
+        this.selectTab = function(checkTab) {
             return this.tab === checkTab;
         };
-
+    });
+    app.controller('GalleryController', function(){
+        this.current = 0;
+        this.setCurrent = function(iCur){
+            this.current = iCur || 0 ;
+        };
+    });
+    app.controller('reviewController', function(){
+        this.review={};
     });
 })();
